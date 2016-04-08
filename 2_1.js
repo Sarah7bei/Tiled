b8d89c24371bacdd65a44c4cbf89eebd2a9072b6
Sarah7bei <458248232@qq.com>
2016-04-08 10:13:49 +0800

    fenzhi

---
+++ 2_1.js
@@ -0,0 +1,30 @@
+//使用字面量来创建对象
+var p = {
+	name:"jackie",
+	_age:"18",
+	set age(val)
+	{
+		console.log("set");
+		this._age=val;
+	},
+	get age()
+	{
+		console.log("get");
+		return this._age;
+	},
+	print:function() {
+		console.log(this.name);
+		console.log(this.age);
+	},
+	phoneNumber:{
+		home:123,
+		phone:456
+	}
+};
+p.age=29;
+console.log(p.age);
+p.print();
+console.log(p.phoneNumber.home);
+var age = "name";
+console.log(p.age);
+console.log(p[age]);
